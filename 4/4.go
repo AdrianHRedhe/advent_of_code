cba334e401c2211dfba2bddfaffb307726102e6d
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2025-03-07 01:58:23 +0100

    feat: aoc day 4 part 1 & 2 completed

--- 4/4.go
+++ 4/4.go
@@ -5,20 +5,18 @@ import (
 	"fmt"
 	"log"
 	"os"
-	//"regexp"
-	// "strings"
+	"strings"
 )
 
 func read_input() []string {
 	file, err := os.Open("input.txt")
+
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
-
 	var lines []string
 
-	// Scan and format lines.
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -32,38 +30,19 @@ func read_input() []string {
 	return lines
 }
 
-func make_matrix(rows []string) [][]string {
-	n, m := len(rows), len(rows[0])
-
-	matrix := make([][]string, m)
-	for j := range m {
-		matrix[j] = make([]string, n)
-	}
-
-	for i, row := range rows {
-		for j, char := range row {
-			matrix[j][i] = string(char)
-		}
-	}
-
-	return matrix
-}
-
 type coord struct {
 	x, y int
 }
 
-func make_matrix_map(rows []string) map[coord]string {
-
-	// X, Y := len(rows), len(rows[0])
+func get_matrix(lines []string) map[coord]string {
 	matrix := make(map[coord]string)
 
-	for i, row := range rows {
-		for j, char := range row {
-			matrix[coord{i, j}] = string(char)
+	for x, line := range lines {
+		chars := strings.Split(line, "")
+		for y, char := range chars {
+			matrix[coord{x, y}] = char
 		}
 	}
-
 	return matrix
 }
 
@@ -71,25 +50,27 @@ func check_all_directions(matrix map[coord]string, start coord, substring string
 	counter := 0
 	x, y := start.x, start.y
 
-	possible_directions := []coord{{1, -1}, {1, 0}, {0, 1}, {0, -1}, {0, 1}, {-1, -1}, {-1, 0}, {-1, 1}}
-	for _, possible_direction := range possible_directions {
-		dx, dy := possible_direction.x, possible_direction.y
-		found_string := ""
-		for i := range substring {
-			found_string += matrix[coord{x + dx*i, y + dy*i}]
-		}
-
-		if found_string == substring {
-			counter += 1
+	for _, dx := range []int{1, 0, -1} {
+		for _, dy := range []int{1, 0, -1} {
+			if !(dx == 0 && dy == 0) {
+				// Possible direction
+				found_string := ""
+				for i := range substring {
+					found_string += matrix[coord{x + dx*i, y + dy*i}]
+				}
+				if found_string == substring {
+					counter += 1
+				}
+			}
 		}
 	}
-
 	return counter
+
 }
 
 func count_all_matches(rows []string, substring string) int {
 	n_matches := 0
-	matrix := make_matrix_map(rows)
+	matrix := get_matrix(rows)
 
 	for xy_pair := range matrix {
 		n_matches += check_all_directions(matrix, xy_pair, substring)
@@ -98,9 +79,42 @@ func count_all_matches(rows []string, substring string) int {
 	return n_matches
 }
 
+func x_mas(rows []string) int {
+	counter := 0
+	matrix := get_matrix(rows)
+
+	for xy_pair := range matrix {
+		counter += check_x_mas(matrix, xy_pair)
+	}
+	return counter
+}
+
+func check_x_mas(matrix map[coord]string, xy coord) int {
+	nw := coord{xy.x + 1, xy.y + 1}
+	ne := coord{xy.x + 1, xy.y - 1}
+	se := coord{xy.x - 1, xy.y - 1}
+	sw := coord{xy.x - 1, xy.y + 1}
+
+	if matrix[xy] != "A" {
+		return 0
+	}
+
+	if !((matrix[nw] == "M" && matrix[se] == "S") || (matrix[nw] == "S" && matrix[se] == "M")) {
+		return 0
+	}
+
+	if !((matrix[ne] == "M" && matrix[sw] == "S") || (matrix[ne] == "S" && matrix[sw] == "M")) {
+		return 0
+	}
+	return 1
+}
+
 func main() {
 	rows := read_input()
 	n_xmas := count_all_matches(rows, "XMAS")
 	// n_xmas := count_xmas(rows)
 	fmt.Println("XMAS counter 1: ", n_xmas)
+
+	x_mas_counter := x_mas(rows)
+	fmt.Println("X-MAS counter 2: ", x_mas_counter)
 }
