517998ed3d87ac3dd4da0c029b7bc11bac484382
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2025-10-01 20:51:16 +0200

    feat: aoc day 20 part 2 completed

--- 20/20.go
+++ 20/20.go
@@ -47,7 +47,15 @@ type state struct {
 	steps int
 }
 
-func getGameMap(lines []string) (gameMap map[coord]string, start coord, end coord, boundary coord) {
+var (
+	// y is from the top hence y: -1 is upwards
+	N = coord{0, -1}
+	W = coord{-1, 0}
+	S = coord{0, 1}
+	E = coord{1, 0}
+)
+
+func getGameMap(lines []string) (gameMap map[coord]string, start coord, end coord) {
 	gameMap = make(map[coord]string)
 	for y, line := range lines {
 		for x, symbol := range line {
@@ -62,17 +70,8 @@ func getGameMap(lines []string) (gameMap map[coord]string, start coord, end coor
 			}
 		}
 	}
-	boundary = coord{x: len(lines[0]), y: len(lines)}
-	return gameMap, start, end, boundary
+	return gameMap, start, end
 }
-
-var (
-	// y is from the top hence y: -1 is upwards
-	N = coord{0, -1}
-	W = coord{-1, 0}
-	S = coord{0, 1}
-	E = coord{1, 0}
-)
 
 func nStepsShortestPath(gameMap map[coord]string, start coord, end coord) int {
 	// init queue
@@ -105,48 +104,99 @@ func nStepsShortestPath(gameMap map[coord]string, start coord, end coord) int {
 	return -1
 }
 
-func findPotentialCheats(gameMap map[coord]string, boundary coord) []coord {
-	xBound, yBound := boundary.x, boundary.y
-	potentialCheats := []coord{}
+func statesPartOfShortestPath(gameMap map[coord]string, start coord, end coord, stepsToEnd int) (states []state) {
+	first := []coord{start}
+	queue := [][]coord{first}
+	seen := make(map[coord]bool)
 
-	for x := range xBound {
-		for y := range yBound {
-			// if x is on the edges then we dont want to cheat
-			if x == 0 || x == xBound {
-				continue
-			}
-			// if y is on the edges then we dont want to cheat
-			if y == 0 || y == yBound {
-				continue
-			}
-			cheat := coord{x: x, y: y}
-			// if it is not a # then it is not a cheat
-			if symbol, exists := gameMap[cheat]; !exists || symbol != "#" {
-				continue
-			}
-			potentialCheats = append(potentialCheats, cheat)
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+
+		// we have gone past the limit should not be requeued
+		if len(current)-1 > stepsToEnd {
+			continue
 		}
+
+		currentPos := current[len(current)-1]
+
+		if currentPos == end {
+			for i, pos := range current {
+				states = append(states, state{pos: pos, steps: i})
+			}
+		}
+
+		for _, dir := range []coord{N, W, S, E} {
+			nextPos := currentPos.move(dir)
+
+			if symbol, exists := gameMap[nextPos]; exists && symbol != "#" {
+				if seen[nextPos] != true {
+					next := append(current, nextPos)
+					queue = append(queue, next)
+					seen[nextPos] = true
+				}
+			}
+		}
+
 	}
-	return potentialCheats
+	return states
 }
 
-func countCheatsThatSave100Steps(gameMap map[coord]string, start coord, end coord, boundary coord) (nCheats int) {
-	var cheatGameMap map[coord]string
-	var cheatSteps int
-	nCheats = 0
-	nStepsOriginal := nStepsShortestPath(gameMap, start, end)
-	stepThreshold := 100
-
-	potentialCheats := findPotentialCheats(gameMap, boundary)
-	for _, cheat := range potentialCheats {
-		cheatGameMap = make(map[coord]string)
-		for k, v := range gameMap {
-			cheatGameMap[k] = v
+func calculateStepsToEndPerPos(gameMap map[coord]string, end coord) map[coord]int {
+	// this is what takes time if we want to speed up this program
+	stepsToEnd := make(map[coord]int)
+	for pos, symbol := range gameMap {
+		if symbol != "#" {
+			stepsToEnd[pos] = nStepsShortestPath(gameMap, pos, end)
 		}
-		cheatGameMap[cheat] = "."
-		cheatSteps = nStepsShortestPath(cheatGameMap, start, end)
-		if (nStepsOriginal - cheatSteps) >= stepThreshold {
-			nCheats++
+	}
+	return stepsToEnd
+}
+
+func generateManhattanPoints(center coord, radius int) []coord {
+	// inspiration: https://stackoverflow.com/questions/75128474/how-to-generate-all-of-the-coordinates-that-are-within-a-manhattan-distance-r-of
+	// generates points in a manhattan radius of a center
+	manhattanPoints := []coord{}
+	for offset := range radius {
+		inverseOffset := radius - offset
+		p1 := coord{x: center.x + offset, y: center.y + inverseOffset}
+		p2 := coord{x: center.x + inverseOffset, y: center.y - offset}
+		p3 := coord{x: center.x - offset, y: center.y - inverseOffset}
+		p4 := coord{x: center.x - inverseOffset, y: center.y + offset}
+		manhattanPoints = append(manhattanPoints, p1)
+		manhattanPoints = append(manhattanPoints, p2)
+		manhattanPoints = append(manhattanPoints, p3)
+		manhattanPoints = append(manhattanPoints, p4)
+	}
+	return manhattanPoints
+}
+
+func teleport(gameMap map[coord]string, from coord, duration int) (to []state) {
+	// jump map, from "from" you can jump to "to.pos" costing "to.steps"
+	to = []state{}
+	for radius := 1; radius <= duration; radius++ {
+		for _, point := range generateManhattanPoints(from, radius) {
+			if symbol, exists := gameMap[point]; exists && symbol != "#" {
+				after := state{pos: point, steps: radius}
+				to = append(to, after)
+			}
+		}
+	}
+	return to
+}
+
+func countCheatsThatSave100Steps(gameMap map[coord]string, start coord, end coord, cheatDuration int, stepsToEnd map[coord]int) (nCheats int) {
+	nCheats = 0
+	stepThreshold := 100
+	numberOfStepsShortestPath := nStepsShortestPath(gameMap, start, end)
+	potentialCheatStates := statesPartOfShortestPath(gameMap, start, end, numberOfStepsShortestPath)
+
+	for _, from := range potentialCheatStates {
+		for _, to := range teleport(gameMap, from.pos, cheatDuration) {
+			cheatSteps := from.steps + to.steps + stepsToEnd[to.pos]
+			if (numberOfStepsShortestPath - cheatSteps) >= stepThreshold {
+				nCheats++
+			}
 		}
 	}
 
@@ -155,10 +205,14 @@ func countCheatsThatSave100Steps(gameMap map[coord]string, start coord, end coor
 
 func main() {
 	lines := read_input()
-	gameMap, start, end, boundary := getGameMap(lines)
-	nCheats := countCheatsThatSave100Steps(gameMap, start, end, boundary)
+	gameMap, start, end := getGameMap(lines)
+	stepsToEnd := calculateStepsToEndPerPos(gameMap, end)
 
+	cheatDuration := 2
+	nCheats := countCheatsThatSave100Steps(gameMap, start, end, cheatDuration, stepsToEnd)
 	fmt.Println("part 1: ", nCheats)
 
-	// fmt.Println("part 2: ", )
+	cheatDuration = 20
+	nCheats = countCheatsThatSave100Steps(gameMap, start, end, cheatDuration, stepsToEnd)
+	fmt.Println("part 2: ", nCheats)
 }
